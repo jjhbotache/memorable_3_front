c4134c99122d7bf394a99a945b3c3261fc03a30a
Juan Jose huertas <jjhuertasbotache@gmail.com>
2024-06-16 00:21:23 -0500

    starting to work on the design section II

--- src/components/design/designContainer/DesignContainer.tsx
+++ src/components/design/designContainer/DesignContainer.tsx
@@ -12,7 +12,7 @@ export default function DesignContainer({designs,arragment}:DesignContainerProps
     
     <DesignsStyledContainer styleBehavior={arragment}>
       {
-        designs.map((design:Desing) => {
+        designs && designs.map((design:Desing) => {
           return(
             <div className="design">
               <img src={design.img_url} alt={design.name}/>
