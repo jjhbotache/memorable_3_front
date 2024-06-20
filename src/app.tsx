7c8f20129a2caa5499c2b6da5b13cb9cd7314a77
Juan Jose huertas <jjhuertasbotache@gmail.com>
2024-06-20 14:19:12 -0500

    trying to fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Navbar from "./components/global/navbar/Navbar";
 import { gapi } from "gapi-script";
-import { googleClientId } from "./constants/appConstants";
+import { API, googleClientId } from "./constants/appConstants";
 import memorableIcon from "./assets/svgs/memorableIcon.svg";
 import Carousel from "./components/landingPage/carousel/Carousel";
 import TagsPresentation from "./components/landingPage/tagsPresentation/TagsPresentation";
 import styled from "styled-components";
+import { toast } from "react-toastify";
 
 export default function App() {
 
@@ -21,6 +22,22 @@ export default function App() {
     }
 
     gapi.load('client:auth2', start);
+
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    if(user){
+      fetch(API + "/user-login-signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        toast.success("Bienvenido de vuelta " + data.user.name);
+      })
+    }
   }, []);
 
 
