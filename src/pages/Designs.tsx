6f3d3d88f72701648e3e48d2cf2015affbb96b4c
juan <jjhuertasbotache@gmail.com>
2024-09-02 01:08:56 -0500

    adding designs displayed

--- src/pages/Designs.tsx
+++ src/pages/Designs.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { primaryColor, secondaryColor } from "../constants/styleConstants"
+import { primaryColor } from "../constants/styleConstants"
 import DesignsContainer from "../components/design/designContainer/DesignContainer"
 import {useState } from "react"
 import Design from "../interfaces/designInterface"
@@ -18,28 +18,23 @@ export default function Designs() {
 
   return(
     <DesignPage>
-      <div className="main">
         <DesignShowerManager arragment={arragmentState} onSwitchArrangment={()=>switchArrangment()} tags={preloadedTags} />
         {/* {loading */}
         {false
         ?<h1>Cargando......</h1>
         :<DesignsContainer designs={designData} arragment={arragmentState} aiSearch={true}/> 
         }
-      </div>
     </DesignPage>  
   )
 };
 
 
+/* src/pages/Designs.tsx */
 const DesignPage = styled.div`
   width: 100%;
-  height: 100%;
-  background: ${secondaryColor};
-  color:${primaryColor};
-  .main{
-    width: 100%;
-    max-width: 1000px;
-    min-height: 100%;
-    margin: 1em auto;
-  }
-  `
+  color: ${primaryColor};
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+
+`;
