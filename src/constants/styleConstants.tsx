f403f19c444a0ab63f0c166ddfdd0b6bef2a101c
juan <jjhuertasbotache@gmail.com>
2024-09-02 21:57:41 -0500

    fixing theme

--- src/constants/styleConstants.tsx
+++ src/constants/styleConstants.tsx
@@ -2,7 +2,7 @@ export const primaryColor = "#531035";
 export const secondaryColor = "#fde4ff";
 export const tertiaryColor = "#efc1da";
 
-export const darkPrimaryColor = "#ffdef0";
+export const darkPrimaryColor = "#fde4ff";
 export const darkSecondaryColor = "#531035";
 export const darkTertiaryColor = "#59243c";
 
